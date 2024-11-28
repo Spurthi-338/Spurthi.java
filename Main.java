2234dc32613e7edda293a482639547e9e58ecbd6
Spurthi-338 <spurthireddy.cs23@bmsce.ac.in>
2024-11-28 10:01:20 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,35 @@
-import CIE.Student;
-import CIE.Internal;
-import SEE.External;
-import java.util.Scanner;
-public class Main {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of students whose details you want to enter");
-		int n = sc.nextInt();
-		Internal[] i1 = new Internal[n];
-		External[] e1 = new External[n];
-		for(int i=0;i<n;i++) {
-			System.out.println("Student "+(i+1)+" details:");
-			e1[i] = new External();
-			i1[i] = new Internal();
-			e1[i].getd();
-			i1[i].getMarks();
-			e1[i].getMarks();
-		}
-		for(int i=0;i<n;i++) {
-			e1[i].display();
-			e1[i].calcTotalMarks(i1[i]);
-		}
-
-	}
+class BMSCollegeThread extends Thread {
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("BMS College of Engineering");
+                Thread.sleep(10000); 
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
+
+class CSEThread extends Thread {
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("CSE");
+                Thread.sleep(2000); 
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        BMSCollegeThread bmsThread = new BMSCollegeThread();
+        CSEThread cseThread = new CSEThread();
+        bmsThread.start();
+        cseThread.start();
+    }
+}
+
